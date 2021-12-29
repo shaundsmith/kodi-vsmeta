0257e28dbfeb33f473bf6756d5abd37bda4fa307
Shaun Smith <shaundsmith3@gmail.com>
2021-12-29 01:34:20 +0000

    Add logging to movie parser

--- get_set_information.py
+++ get_set_information.py
@@ -1,6 +1,8 @@
 import json
 import os.path
 
+import xbmc
+
 SET_INFO_FILE = ".setinfo"
 SET_POSTER = "set-poster.jpg"
 SET_FANART = "set-backdrop.jpg"
@@ -24,17 +26,20 @@ def get(file_path, image_directory):
     file_name = os.path.splitext(os.path.basename(file_path))[0]
     directory = os.path.dirname(file_path)
     set_information_file = os.path.join(directory, SET_INFO_FILE)
-    print("Searching for set information in " + str(set_information_file) + " for " + file_name)
+    xbmc.log(f"Searching for set information in {set_information_file} for {file_name}", xbmc.LOGINFO)
     if os.path.exists(set_information_file):
+        xbmc.log(f"Found set information for {file_name}", xbmc.LOGINFO)
         file_handle = open(set_information_file, "rb")
         raw_set_information = json.load(file_handle)
         file_handle.close()
 
         set_information.title = raw_set_information["title"]
         set_information.summary = raw_set_information["summary"]
+
         if "order" in raw_set_information and file_name in raw_set_information["order"]:
             sort_index = str(raw_set_information["order"].index(file_name)).rjust(2, '0')
             set_information.sort_title = raw_set_information["title"] + sort_index
+
     if os.path.exists(os.path.join(directory, image_directory, SET_POSTER)):
         set_information.poster = os.path.join(directory, image_directory, SET_POSTER)
     if os.path.exists(os.path.join(directory, image_directory, SET_POSTER)):
